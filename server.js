68dc7a608073ac807c857819e94a3ffc762cb49d
NoSock <nosock93@gmail.com>
2018-06-26 13:42:12 +0300

    simplified server.js

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const publicPath = __dirname + '/dist/superiorvk';
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname+'/dist/superiorvk/index.html'))
-)
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/superiorvk' + req.baseUrl));
-});
+app.use(express.static(publicPath));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
