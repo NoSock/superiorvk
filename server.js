5198794efcb4eb91dd0ad1fa4a53be3ae8503fe3
NoSock <nosock93@gmail.com>
2018-06-26 13:55:12 +0300

    added default route.... again

--- server.js
+++ server.js
@@ -5,8 +5,25 @@ const publicPath = __dirname + '/dist/superiorvk';
 
 const app = express();
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
+
 // Serve only the static files form the dist directory
 app.use(express.static(publicPath));
+
+app.get('/*', function (req, res) {
+  res.sendFile(publicPath);
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
